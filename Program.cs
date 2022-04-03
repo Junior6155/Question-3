a3c791907a26b4b2507978cb836221d5959b1637
Junior <82260509+Junior6155@users.noreply.github.com>
2022-04-03 00:04:03 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Coursework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Getting the inputs from the user
+            Console.WriteLine("Enter the hourly wage: ");
+
+            //Converting the string value to an integer value
+            int hourlyWage = Convert.ToInt32(Console.ReadLine());
+
+            // Getting the inputs from the user
+            Console.WriteLine("Enter the total regular hours: ");
+            
+            // Total regular hours is trh
+            int trh = Convert.ToInt32(Console.ReadLine());
+
+            
+            // Getting the inputs from the user
+            Console.WriteLine("Enter the total overtime hours: ");
+
+            // Total ovetime hours is toh
+            int toh = Convert.ToInt32(Console.ReadLine());
+
+            // Using double and implementing the arithmetic operations
+            double totalWeeklyPay = (hourlyWage * trh) + (toh * 1.5);
+
+            // Print out the employee's total weekly pay
+            Console.WriteLine("The employee's total weekly pay " + totalWeeklyPay);
+
+
+        }
+    }
+}
